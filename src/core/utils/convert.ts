4cdc4e2faf4a9b161382859b9e0498bb92905cf7
WindrunnerMax <651525974@qq.com>
2022-12-03 11:12:41 +0800

    fix: mxCodec decode bug

--- src/core/utils/convert.ts
+++ src/core/utils/convert.ts
@@ -1,15 +1,15 @@
-import mx from "./mxgraph";
+import { mxGraph, mxCodec } from "./mxgraph";
 import { stringToXml } from "./xml";
 
 export const convertToSVG = (str: string): SVGElement | null => {
   const element = document.createElement("div");
   const doc = stringToXml(str);
   if (doc) {
-    const graph = new mx.mxGraph(element);
+    const graph = new mxGraph(element);
     graph.htmlLabels = true;
     graph.cellsEditable = false;
     graph.model.beginUpdate();
-    const codec = new mx.mxCodec(doc);
+    const codec = new mxCodec(doc);
     codec.decode(doc.documentElement, graph.getModel());
     graph.model.endUpdate();
     const svg = element.firstChild ? element.firstChild.cloneNode(true) : null;
--- src/core/utils/mxgraph.ts
+++ src/core/utils/mxgraph.ts
@@ -21,10 +21,30 @@ const mx = factory({
   mxBasePath: "static",
 });
 
-for (const item of Object.keys(mx)) {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  window[item] = mx[item];
-}
+// 需要用到的再引用 实际上还是把所有的包都打进来了
+export const { mxGraph, mxCodec, mxConstants } = mx;
 
-export default mx;
+// https://github.com/maxGraph/maxGraph/issues/102
+// https://github.com/jgraph/mxgraph/blob/master/javascript/src/js/io/mxCodec.js#L423
+mxCodec.prototype.decode = function (node, into) {
+  this.updateElements();
+  let obj: unknown = null;
+  if (node && node.nodeType == mxConstants.NODETYPE_ELEMENT) {
+    let ctor: unknown = null;
+    try {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore // 因为需要处理的`XML Node`可能不在`Window`上
+      ctor = mx[node.nodeName] || window[node.nodeName];
+    } catch (error) {
+      console.log(`NODE ${node.nodeName} IS NOT FOUND`, error);
+    }
+    const dec = mx.mxCodecRegistry.getCodec(ctor);
+    if (dec) {
+      obj = dec.decode(this, node, into);
+    } else {
+      obj = node.cloneNode(true);
+      obj && (obj as Element).removeAttribute("as");
+    }
+  }
+  return obj;
+};
