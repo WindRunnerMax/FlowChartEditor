133e1dc3ad758015f6d0d999943d4b9b1c404a71
WindrunnerMax <651525974@qq.com>
2022-12-17 13:02:27 +0800

    fix: 配置页面大小

--- src/packages/editor/index.ts
+++ src/packages/editor/index.ts
@@ -12,26 +12,9 @@ mxWindow.prototype.maximizeImage = maximize;
 mxWindow.prototype.minimizeImage = minimize;
 mxWindow.prototype.resizeImage = resize;
 
-export { Actions, Action } from "./js/Actions";
-export { ColorDialog, OutlineWindow, LayersWindow } from "./js/Dialogs";
-
-export { FilenameDialog, ErrorDialog, Editor, Dialog } from "./js/Editor";
-
-export { ChangePageSetup, EditorUi } from "./js/EditorUi";
-
-export {
-  StyleFormatPanel,
-  TextFormatPanel,
-  Format,
-  ArrangePanel,
-  BaseFormatPanel,
-  DiagramFormatPanel,
-} from "./js/Format";
-
-export { TableLayout, Graph, HoverIcons } from "./js/Graph";
-export { Menubar, Menu, Menus } from "./js/Menus";
-export { Sidebar } from "./js/Sidebar";
-export { Toolbar } from "./js/Toolbar";
+export { Editor } from "./js/Editor";
+export { EditorUi } from "./js/EditorUi";
+export { Graph } from "./js/Graph";
 
 // python3 -m http.server 9000
 // https://github.com/jgraph/mxgraph-js f43fc06f72ff5153e84ebfab8eed16a7d58a3b68
